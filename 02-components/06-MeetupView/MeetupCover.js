f5e4fa97917e5a9182632b02f3b529fb3a932786
AlexanderBredun <anozzeleron.work@gmail.com>
2022-01-31 14:26:46 +0200

    Solved MeetupView

---
+++ 02-components/06-MeetupView/MeetupCover.js
@@ -0,0 +1,12 @@
+import { defineComponent } from './vendor/vue.esm-browser.js';
+
+export default defineComponent({
+  name: 'MeetupCover',
+  props:['title', 'image'],
+
+
+  template: `
+    <div class="meetup-cover" :style="{'--bg-url': image ? 'url(' + image + ')' : null}">
+        <h1 class="meetup-cover__title">{{ title }}</h1>
+    </div>`,
+});
