d0bba60b700feb25b0eea3ec2d3f66b76a0cc21f
AlexanderBredun <anozzeleron.work@gmail.com>
2022-01-31 12:20:26 +0200

    Solved Компонент CounterButton

--- 02-components/01-CounterButtonComponent/CounterButton.js
+++ 02-components/01-CounterButtonComponent/CounterButton.js
@@ -4,9 +4,19 @@ export default defineComponent({
   name: 'CounterButton',
 
   // Компонент должен иметь входной параметр
+  props: {
+    count: {
+      default: 0
+    }
+  },
 
   // Шаблон лучше держать максимально простым, а логику выносить в методы
+  methods: {
+    updateCount(){
+      this.$emit('update:count', this.$props.count + 1);
+    }
+  },
 
   // Шаблон потребуется отредактировать
-  template: `<button type="button">1</button>`,
+  template: `<button type="button" @click.prevent="updateCount">{{ count }}</button>`,
 });
